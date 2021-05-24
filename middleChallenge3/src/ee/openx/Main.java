e90437805d360fcbabdbc13c1c19ef7490f37972
Riza Ozer <rizaoozer@gmail.com>
2021-05-25 00:20:59 +0300

    start work

--- middleChallenge3/src/ee/openx/Main.java
+++ middleChallenge3/src/ee/openx/Main.java
@@ -5,33 +5,34 @@ import java.util.Arrays;
 public class Main {
 
         public static void main(String[] args) {
-            int[] nums = { 1, 1, 2, 3, 3, 4, 5, 2, 4, 5, 6, 7, 8, 9, 6, -1, -2 };
-            System.out.println("\nOriginal array: "+ Arrays.toString(nums));
+            int[] nums = {100, 4, 200, 1, 3, 2};
             System.out.println(findLongestConsecutiveSequence(nums));
+
         }
 
         public static int findLongestConsecutiveSequence(int[] nums) {
+            int longestConsecutiveSequence = 0;
 
-            boolean flag = false;
-            int result = 0, start = 0, end = 0;
-            for (int i = 1; i < nums.length; i++) {
-                if (nums[i - 1] < nums[i]) {
-                    end = i;
-                } else {
-                    start = i;
+            for (int i = 0; i < nums.length; i++) {
+                int currentNumber = nums[i];
+                int currentConsecutiveSequence = 1;
+                int currentConsecutiveLength = 1;
+
+                while(numberExists(nums, currentNumber + 1)) {
+                    currentNumber++;
+                    currentConsecutiveLength++;
                 }
-                if (end - start > result) {
-                    flag = true;
-                    result = end - start;
+                longestConsecutiveSequence = Math.max(longestConsecutiveSequence, currentConsecutiveSequence);
+            }
+            return longestConsecutiveSequence;
+        }
+
+        public static boolean numberExists(int[] array, int number) {
+            for (int i = 0; i < array.length; i++) {
+                if(array[i] == number) {
+                    return true;
                 }
             }
-            if (flag)
-            {
-                return result + 1;
-            }
-            else
-            {
-                return result;
-            }
+            return false;
         }
     }
