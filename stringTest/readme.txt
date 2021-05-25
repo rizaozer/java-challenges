6e8a6f47a241209f0a077ac3933e72a8436c79fa
Riza Ozer <rizaoozer@gmail.com>
2021-05-25 21:08:21 +0300

    create files stringTest

---
+++ stringTest/readme.txt
@@ -0,0 +1,5 @@
+Write a Java program to accept two string and test if the second string contains the first one.
+
+Input first string: Once in a blue moon
+Input second string: See eye to eye
+If the second string contains the first one? false
---
+++ stringTest/src/ee/openx/Main.java
@@ -0,0 +1,31 @@
+package ee.openx;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+        System.out.println("Input first string: ");
+        String firstString = scanner.nextLine();
+        System.out.println("Input second string: ");
+        String secondString = scanner.nextLine();
+        System.out.println(isContainsString(firstString, secondString));
+    }
+    
+    public static boolean isContainsString(String firstString, String secondString) {
+        boolean ans = false;
+        for (int i = 0; i < secondString.length(); i++) {
+            if(secondString.charAt(i) == firstString.charAt(0)) {
+                for (int j = 0; j < firstString.length() ; j++) {
+                    if ((i + j) < secondString.length() && firstString.charAt(j) == secondString.charAt(i + j) &&
+                            j == firstString.length() - 1) {
+                        ans = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
