383cd67906f9f0f0b53d3e2cb3cd6ca3f6f425d8
Riza Ozer <rizaoozer@gmail.com>
2021-05-25 11:07:24 +0300

    finish selectionSort

---
+++ sortingAlgorithms/selectionSort/src/ee/openx/Main.java
@@ -0,0 +1,30 @@
+package ee.openx;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int [] numbersArray = {2, 1, 12, 5, 100, -1, 50, 20, -2, 15};
+        System.out.println("Original Array: " + Arrays.toString(numbersArray));
+        System.out.println("Sorted Array: " + Arrays.toString(selectionSort(numbersArray)));
+    }
+
+    public static int[] selectionSort(int [] array) {
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if(array[j] < array[minIndex]) {
+                    swap(array, j, minIndex);
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void swap(int[] array, int first, int second) {
+        int temp = array[first];
+        array[first] = array[second];
+        array[second] = temp;
+    }
+}
