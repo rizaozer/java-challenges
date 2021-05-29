9f4ba952353fe818ec3938ace155e792fa03d348
Riza Ozer <rizaoozer@gmail.com>
2021-05-29 15:23:18 +0300

    finish numberOfBoomerangs

---
+++ numberOfBoomerangs/readme.txt
@@ -0,0 +1,14 @@
+Number of Boomerangs
+A boomerang is a V-shaped sequence that is either upright or upside down.
+Specifically, a boomerang can be defined as: sub-array of length 3, with the first and last digits
+being the same and the middle digit being different.
+
+Some boomerang examples:
+
+[3, 7, 3], [1, -1, 1], [5, 6, 5]
+Create a function that returns the total number of boomerangs in an array.
+
+To illustrate:
+
+[3, 7, 3, 2, 1, 5, 1, 2, 2, -2, 2]
+// 3 boomerangs in this sequence:  [3, 7, 3], [1, 5, 1], [2, -2, 2]
---
+++ numberOfBoomerangs/src/ee/openx/Main.java
@@ -0,0 +1,20 @@
+package ee.openx;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbersArray = {3, 7, 3, 1, -1, 1, 5, 6, 5};
+        
+        System.out.println(countBomeranges(numbersArray));
+    }
+
+    private static int countBomeranges(int[] array) {
+        int counter = 0;
+        for (int i = 0; i < array.length - 2; i++) {
+            if(array[i] == array[i + 2] && array[i] != array[i + 1]) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
