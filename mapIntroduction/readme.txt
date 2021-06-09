6be95ac1a16e461ad2e5a8976694660a4fe7b2e7
Riza Ozer <rizaoozer@gmail.com>
2021-06-09 15:28:56 +0300

    write basics of HashMap Creation

---
+++ mapIntroduction/readme.txt
@@ -0,0 +1,8 @@
+Map-1 > topping1
+
+Given a map of food keys and topping values, modify and return the map as follows: if the key "ice cream" is present, set its value to "cherry". In all cases, set the key "bread" to have the value "butter".
+
+
+topping1({"ice cream": "peanuts"}) → {"bread": "butter", "ice cream": "cherry"}
+topping1({}) → {"bread": "butter"}
+topping1({"pancake": "syrup"}) → {"bread": "butter", "pancake": "syrup"}
--- mapIntroduction/src/ee/openx/Main.java
+++ mapIntroduction/src/ee/openx/Main.java
@@ -1,8 +1,26 @@
 package ee.openx;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //1. Creating HashMap with default initial capacity and load factor
+        HashMap<String, Integer> map1 = new HashMap<String, Integer>();
+
+        //2. Creating HashMap with 30 as initial capacity
+        HashMap<String, Integer> map2 = new HashMap<String, Integer>(30);
+
+        //3. Creating HashMap with 30 as initial capacity and 0.5 as load factor
+        HashMap<String, Integer> map3 = new HashMap<String, Integer>(30, 0.5f);
+
+        //4. Creating HashMap by copying another HashMap
+        HashMap<String, Integer> map4 = new HashMap<String, Integer>(map1);
+    }
+
+    public Map<String, String> topping1(Map<String, String> map) {
+        
     }
 }
