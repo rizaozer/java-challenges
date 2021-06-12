a06b0a907a19f29f125225d0467fbe879ad33d28
Riza Ozer <rizaoozer@gmail.com>
2021-06-13 00:51:53 +0300

    find first palindrometime

--- PalindromeTimestamps/src/ee/openx/PalindromeTimeStamps.java
+++ PalindromeTimestamps/src/ee/openx/PalindromeTimeStamps.java
@@ -2,12 +2,29 @@ package ee.openx;
 
 public class PalindromeTimeStamps {
     public static void main(String[] args) {
-
+        palindromeTimestamps(2, 12, 22, 10, 35, 4);
     }
 
     public static void palindromeTimestamps(int hour1, int minute1, int second1, int second2, int minute2, int hour2) {
-        for (int hour = hour1; hour < hour2; hour++) {
 
+        boolean flag = true;
+        while (flag) {
+            second1++;
+            if(hour1 == second1 && minute1 % 10 == minute1 / 10 && minute1 != 0) {
+                System.out.println(hour1 + " : " + minute1 + " : " + second1);
+            }
+            if (second1 == 60) {
+                minute1++;
+                second1 = 0;
+                if(minute1 == 60) {
+                    minute1 = 0;
+                    hour1++;
+
+                    if(hour1 == hour2) {
+                            flag = false;
+                    }
+                }
+            }
         }
     }
 }
--- PalindromeTimestamps/src/ee/openx/test.java
+++ PalindromeTimestamps/src/ee/openx/test.java
@@ -2,49 +2,30 @@ package ee.openx;
 
 public class test {
 
-        // Function to get the required minutes
         public static int get_palindrome_time(String str) {
-            int hh;
-            int mm;
+            int hour;
+            int minute;
 
-            // Storing hour and minute value
-            // in integral form
-            // String str = "05:39";
-            hh = Integer.parseInt(str.substring(0, 2));
-            mm = Integer.parseInt(str.substring(3, 5));
+            hour = Integer.parseInt(str.substring(0, 2));
+            minute = Integer.parseInt(str.substring(3, 5));
             int requiredTime = 0;
 
-            // Keep iterating till first digit
-            // hour becomes equal to second
-            // digit of minute and second digit
-            // of hour becomes equal to first
-            // digit of minute
-            while (hh % 10 != mm / 10 || hh / 10 != mm % 10) {
-                ++mm;
-
-                // If mins is 60, increase hour, and
-                // reinitilialized to 0
-                if (mm == 60) {
-                    mm = 0;
-                    ++hh;
+            while (hour % 10 != minute / 10 || hour / 10 != minute % 10) {
+                ++minute;
+                if (minute == 60) {
+                    minute = 0;
+                    ++hour;
                 }
-
-                // If hours is 60, reinitialized to 0
-                if (hh == 24) {
-                    hh = 0;
+                if (hour == 24) {
+                    hour = 0;
                 }
                 ++requiredTime;
             }
-
-            // Return the required time
             return requiredTime;
         }
 
         public static void main(String[] args) {
-            // Given Time as a string
             String str = "05:39";
-
-            // Function Call
             System.out.println(get_palindrome_time(str));
         }
     }
