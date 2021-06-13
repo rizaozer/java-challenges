1c4d16cc0236beb5fd8cb63059591fd56db91e6d
Riza Ozer <rizaoozer@gmail.com>
2021-06-13 22:41:53 +0300

    finish closingInSum

--- closingInSum/src/ee/openx/Main.java
+++ closingInSum/src/ee/openx/Main.java
@@ -3,11 +3,14 @@ package ee.openx;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(closingInSum("22155"));
+        System.out.println(closingInSum("2255"));
     }
 
     public static int closingInSum(String stringNumber) {
-        if(stringNumber.length() <= 1) {
+        if(stringNumber.length()== 0) {
+            return 0;
+        }
+        if(stringNumber.length() == 1) {
             return Integer.parseInt(stringNumber);
         } else {
             return Integer.parseInt(stringNumber.substring(0, 1) + stringNumber.substring(stringNumber.length() - 1)) +
