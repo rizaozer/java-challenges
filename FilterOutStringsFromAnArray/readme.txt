8d3248e3d19ecfb144117daa3547ad146bb82df6
Riza Ozer <rizaoozer@gmail.com>
2021-06-28 21:25:58 +0300

    FilterOutStringsFromAnArray

--- FilterOutStringsFromAnArray/readme.txt
+++ FilterOutStringsFromAnArray/readme.txt
@@ -2,11 +2,11 @@ Filter out Strings from an Array
 Create a function that takes an array of non-negative integers and strings and return a new array without the strings.
 
 Examples
-filterArray([1, 2, "a", "b"]) ➞ [1, 2]
+filterArray([1, 2, "a", "b"]) ➞ [1, 2, 0]
 
 filterArray([1, "a", "b", 0, 15]) ➞ [1, 0, 15]
 
-filterArray([1, 2, "aasf", "1", "123", 123]) ➞ [1, 2, 123]
+filterArray([1, 2, "aasf", "1", "123", 123]) ➞ [1, 2, 0, 123]
 Notes
 Zero is a non-negative integer.
 The given array only has integers and strings.
--- FilterOutStringsFromAnArray/src/ee/openx/Main.java
+++ FilterOutStringsFromAnArray/src/ee/openx/Main.java
@@ -1,29 +1,45 @@
 package ee.openx;
 
-import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         List<Object> sections = new ArrayList <>();
-        sections.add(1);
+        sections.add("21s0sd3");
         sections.add(2);
         sections.add("aasf");
         sections.add("1");
+        sections.add("2");
         sections.add("123");
         sections.add(123);
-        System.out.println(Arrays.toString(filterArray(sections)));
+        System.out.println(filterArray(sections));
     }
 
-    public static int [] filterArray(List<Object> sections) {
-        int [] numbersArray = new int[1];
-        numbersArray[0] = (int) sections.get(0);
-//        for (int i = 0; i < sections.size(); i++) {
-//           sections.get(i);
-//        }
-        return numbersArray;
+    public static List<Integer> filterArray(List<Object> sections) {
+        List<Integer> numbers = new ArrayList <>();
+        for (int i = 0; i < sections.size(); i++) {
+            String myString = "";
+            for (int j = 0; j < sections.get(i).toString().length(); j++) {
+                if(Character.isDigit(sections.get(i).toString().charAt(j))) {
+                    myString = myString + sections.get(i).toString().charAt(j);
+                }
+            }
+            int myNumber = 0;
+            boolean flag = false;
+            for (int k = 0; k < myString.length(); k++) {
+                if(Character.isDigit(myString.charAt(k))) {
+                    flag = true;
+                }
+            }
+            if(flag) {
+                myNumber = Integer.parseInt(myString);
+            }
+            if(!numbers.contains(myNumber)) {
+                numbers.add(myNumber);
+            }
+        }
+        return numbers;
     }
 }
