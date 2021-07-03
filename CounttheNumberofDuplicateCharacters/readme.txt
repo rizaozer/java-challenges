0ffd9cca27523163f11600d8977976067455523a
Riza Ozer <rizaoozer@gmail.com>
2021-07-04 00:42:47 +0300

    modify CounttheNumberofDuplicateCharacters and change the readme.txt

--- CounttheNumberofDuplicateCharacters/readme.txt
+++ CounttheNumberofDuplicateCharacters/readme.txt
@@ -1,15 +1,14 @@
 Count the Number of Duplicate Characters
-Create a function that takes a string and returns the number of alphanumeric characters that occur more than once.
+Create a function that takes a string and returns the number of dublicate characters.
 
 Examples
 duplicateCount("abcde") ➞ 0
 
 duplicateCount("aabbcde") ➞ 2
 
-duplicateCount("Indivisibilities") ➞ 2
+duplicateCount("Indivisibilities") ➞ 1
 
 duplicateCount("Aa") ➞ 0
 // Case sensitive
 Notes
 Duplicate characters are case sensitive.
-The input string will contain only alphanumeric characters.
--- CounttheNumberofDuplicateCharacters/src/ee/openx/Main.java
+++ CounttheNumberofDuplicateCharacters/src/ee/openx/Main.java
@@ -3,23 +3,28 @@ package ee.openx;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(duplicateCount("abcadacezz")); // -> 3
-        System.out.println(duplicateCount("Indivisibilities")); // -> 2
-        System.out.println(duplicateCount("Aa")); // -> 0
+//        System.out.println(duplicateCount("aabbcadea")); // -> 3
+//        System.out.println(duplicateCount("aiiuaiddudrizz")); // -> 2
+        System.out.println(duplicateCount("Indivisibilities")); // -> 0
     }
 
     public static int duplicateCount(String myText) {
-        int counter = 0;
+        int count = 0;
+        int tempCount = 1;
         for (int i = 0; i < myText.length(); i++) {
-            char temp = myText.charAt(i);
+            i = 0;
             for (int j = i + 1; j < myText.length(); j++) {
-                if (temp == myText.charAt(j)) {
-                    counter++;
-                    myText = myText.replaceAll(myText.substring(j, j + 1), "");
-                    break;
+                if (myText.charAt(i) == myText.charAt(j)) {
+                    tempCount++;
                 }
             }
+            System.out.println(myText + " " + count);
+            if(tempCount == 2) {
+                count++;
+            }
+            myText = myText.replaceAll(myText.substring(i, i + 1), "");
+            tempCount = 1;
         }
-        return counter;
+        return count;
     }
 }
