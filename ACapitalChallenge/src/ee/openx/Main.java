626daa5107715f2c5778af9fc97fb1ec7854940f
Riza Ozer <rizaoozer@gmail.com>
2021-07-20 13:40:53 +0300

    fix code design

--- ACapitalChallenge/src/ee/openx/Main.java
+++ ACapitalChallenge/src/ee/openx/Main.java
@@ -9,21 +9,17 @@ public class Main {
     public static String selectLetters(String text1, String text2) {
         String myText = "";
         for (int i = 0; i < text2.length(); i++) {
-            if(i >= text1.length()) {
+            if(i >= text1.length())
                 break;
-            }
-            if(Character.isUpperCase(text2.charAt(i))) {
+            if(Character.isUpperCase(text2.charAt(i))) 
                 myText = myText + text1.charAt(i);
-            }
         }
 
         for (int i = 0; i < text1.length(); i++) {
-            if(i >= text2.length()) {
+            if(i >= text2.length())
                 break;
-            }
-            if(Character.isUpperCase(text1.charAt(i))) {
+            if(Character.isUpperCase(text1.charAt(i)))
                 myText = myText + text2.charAt(i);
-            }
         }
         return myText;
     }
