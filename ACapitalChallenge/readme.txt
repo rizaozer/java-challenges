8cbf4b4131dd5fa6fadd1dc1879569f45eb75836
Riza Ozer <rizaoozer@gmail.com>
2021-07-20 13:39:12 +0300

    finish ACapitalChallenge

---
+++ ACapitalChallenge/readme.txt
@@ -0,0 +1,17 @@
+A Capital Challenge
+Given two strings, s1 and s2, select only the characters in each string where the character
+in the same position in the other string is in uppercase. Return these as a single string.
+
+To illustrate, given the strings s1 = "heLLo" and s2 = "GUlp", we select the letters "he" from s1,
+because "G" and "U" are uppercase. We then select "lp" from s2, because "LL" is in uppercase.
+Finally, we join these together and return "help".
+
+Examples
+selectLetters("heLLO", "GUlp") ➞ "help"
+
+selectLetters("1234567", "XxXxX")  ➞ "135"
+
+selectLetters("EVERYTHING", "SomeThings") ➞  "EYSomeThings"
+Notes
+The strings don't have to be the same length.
+Strings can contain non-alphabetic characters.
---
+++ ACapitalChallenge/src/ee/openx/Main.java
@@ -0,0 +1,30 @@
+package ee.openx;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(selectLetters("heLLO", "GUlp")); // -> help
+    }
+
+    public static String selectLetters(String text1, String text2) {
+        String myText = "";
+        for (int i = 0; i < text2.length(); i++) {
+            if(i >= text1.length()) {
+                break;
+            }
+            if(Character.isUpperCase(text2.charAt(i))) {
+                myText = myText + text1.charAt(i);
+            }
+        }
+
+        for (int i = 0; i < text1.length(); i++) {
+            if(i >= text2.length()) {
+                break;
+            }
+            if(Character.isUpperCase(text1.charAt(i))) {
+                myText = myText + text2.charAt(i);
+            }
+        }
+        return myText;
+    }
+}
