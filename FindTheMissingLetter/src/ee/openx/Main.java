b48b91102220a0440c774968b880a9268db00a9c
Riza Ozer <rizaoozer@gmail.com>
2021-07-22 00:18:15 +0300

    finish FindTheMissingLetter

--- FindTheMissingLetter/src/ee/openx/Main.java
+++ FindTheMissingLetter/src/ee/openx/Main.java
@@ -1,10 +1,25 @@
 package ee.openx;
 
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        char [] myChars = {'a', 'b', 'c', 'e', 'f', 'g'};
+        System.out.println(missingLetter(myChars));
     }
+
+    public static char missingLetter(char [] charArray) {
+        for (int i = 0; i < charArray.length - 1; i++) {
+            if (charArray[i + 1] - charArray[i] != 1) {
+                int c = charArray[i] + 1;
+                char missingChar = (char) c;
+                return missingChar;
+            }
+        }
+        return 0;
+    }
+
+
 
 
 }
