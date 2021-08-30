8b9267c130aa838259d4325f80efb42d05e29b0f
rizaozer <rizaoozer@gmail.com>
2021-08-31 00:12:10 +0300

    solved

--- getTheLuckyNumber/src/com/openx/Main.java
+++ getTheLuckyNumber/src/com/openx/Main.java
@@ -3,15 +3,15 @@ package com.openx;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(getLuckyNumber(3, 7));
+        System.out.println(getLuckyNumber(41, 50));
     }
 
     public static int getLuckyNumber(int start, int end) {
         int counter = 0;
         for (int i = start; i <= end; i++) {
             boolean flag = true;
-            for (int j = 2; j < end; j++) {
-                if(i / j == 0 && i != j) {
+            for (int j = 2; j <= end; j++) {
+                if(i % j == 0 && i != j) {
                     flag = false;
                 }
             }
